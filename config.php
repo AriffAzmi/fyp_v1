f27c0ab09cae4765f699d960bf75d1ff67c68173
AriffAzmi <ariff.azmi16@gmail.com>
2015-08-20 01:04:54 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php 
-
-	$isms_username = 'Ariff Azmi'; 
-	$isms_password = 'arip6056'; 
-	$isms_receiver = '60149324340';
+	//Sending sms alert after complete booking
+	$isms_username = ''; 
+	$isms_password = ''; 
+	$isms_receiver = '';
 
 	$servername = "localhost";
-	$username	= "root";
-	$password	= "acv2077";
-	$dbname 	= "cfgs";
+	$username	= "";
+	$password	= "";
+	$dbname 	= "";
 
 	$mysqli = new mysqli($servername, $username, $password, $dbname);
 
